c7c8d03ef23f9ef96ba7bd086e8851c608f78a68
nickobrad <nicholas.omondi@student.moringaschool.com>
2021-08-17 11:29:50 +0300

    migration one

---
+++ migrations/versions/5c1a18660d69_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3e771feb952e
+Revises: 
+Create Date: 2021-08-14 04:32:52.833760
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3e771feb952e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
